62d3e7476b5be29f41395ce44db47c36f392eb5e
Lital Levin <lital.levin@gmail.com>
2020-02-12 13:27:56 +0100

    controller actions

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+patchanka = { name: "Patchanka", address: " 33-35 Rue Saint-Sébastien, 75011 Paris", category: "French", rating: 5 }
+comptoir = { name: "Le Comptoir de la Gastronomie", address: "34 rue Montmartre", category: "French", rating: 5 }
+philo = { name: "Les Philosophes", address: "28 rue Vieille du Temple, 75004 Paris", category: "Belgian", rating: 4 }
+sanukiya = { name: "Sanukiya", address: "9 rue d'Argenteuil, 75001 Paris", category: "japanese", rating: 4 }
+coincidence = { name: "La Coincidence", address: "18 rue Mesnil, 75116 Paris", category: "Italian", rating: 3 }
+
+
+[ patchanka, comptoir, philo, sanukiya, coincidence ].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
