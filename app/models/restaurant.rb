7b867d0106ec49a7a533e10e8fda6aae4b911272
Lital Levin <lital.levin@gmail.com>
2020-02-12 12:16:49 +0100

    validations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,11 @@
 class Restaurant < ApplicationRecord
+
+  CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
+
+  validates :name, :address, :category, presence: true
+  validates :category, inclusion: { in: CATEGORIES,
+    message: "%{value} is not a valid category" }
+  has_many :reviews, dependent: :destroy
+
+
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,8 @@
 class Review < ApplicationRecord
+  RATING = (0..5)
+
+  validates :content, :rating, presence: true
+  validates :rating, numericality: { only_integer: true }
+  validates :rating, inclusion: { in: RATING }
   belongs_to :restaurant
 end
